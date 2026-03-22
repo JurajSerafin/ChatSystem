e2aca9c91334bfdf0d2a5514e5d9ff50f3883c8a
Juraj Serafin <79434395@cuni.cz>
2026-03-22 09:59:02 +0100

    Define Chat class API

--- project/src/ChatSystem/Server/Include/Chat/chat.h
+++ project/src/ChatSystem/Server/Include/Chat/chat.h
@@ -1,8 +1,56 @@
 #ifndef CHAT_H
 #define CHAT_H
 
-class Chat {
+#include <chrono>
+#include <optional>
+#include <shared_mutex>
+#include <unordered_map>
+#include <vector>
 
+#include "Id/base_id.h"
+#include "chat_id.h"
+
+#include <Message/message.h>
+#include <Message/message_id.h>
+#include <User/user.h>
+#include <User/user_id.h>
+
+class Chat {
+private:
+    using time_point = std::chrono::system_clock::time_point;
+
+    std::vector<User> participants_;
+
+    std::unordered_map<UserId, User, BaseId<UserId>::Hasher> user_id_map_;
+    
+    ChatId id_;
+
+    time_point creation_point_;
+
+    std::optional<Message> last_message_;
+
+    mutable std::shared_mutex mutex_;
+
+public:
+
+    Chat(ChatId&& id, time_point created_at) : id_(std::move(id)), creation_point_(created_at) {}
+
+    [[nodiscard]] const std::vector<User>& GetParticipants() const;
+
+    [[nodiscard]] bool HasUser(const UserId& id) const;
+
+    [[nodiscard]] time_point CreatedAt() const;
+
+    [[nodiscard]] bool HasLastMessage() const;
+
+    [[nodiscard]] const std::optional<Message> GetLastMessage() const;
+
+    bool AddParticipant(const User& newParticipant);
+
+    bool RemoveUserById(const UserId& userId);
+
+    void UpdateLastMessage(const Message& message);
+    
 };
 
 #endif // CHAT_H
