8cd09a8ff2f79b3377bfd81caa54f8793b98149c
Juraj Serafin <79434395@cuni.cz>
2026-03-22 11:22:20 +0100

    Define RawMessage

--- project/src/ChatSystem/Server/Include/Message/raw_message.h
+++ project/src/ChatSystem/Server/Include/Message/raw_message.h
@@ -1,8 +1,25 @@
 #ifndef RAW_MESSAGE_H
 #define RAW_MESSAGE_H
 
-class RawMessage {
+#include <chrono>
+#include <string>
 
+#include <Message/message_type.h>
+#include <User/user_id.h>
+#include <Chat/chat_id.h>
+
+
+struct RawMessage {
+    UserId sender_id;
+
+    ChatId chat_id;
+
+    std::string content;
+
+    MessageType type;
+
+    std::chrono::system_clock::time_point timestamp;
 };
+
 
 #endif // RAW_MESSAGE_H
