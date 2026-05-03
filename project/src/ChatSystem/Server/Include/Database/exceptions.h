4246cbc88b3b93ba250417dfe70bc5a0ffada5b0
Juraj Serafin <79434395@cuni.cz>
2026-05-03 11:58:58 +0200

    Create exceptions include file

---
+++ project/src/ChatSystem/Server/Include/Database/exceptions.h
@@ -0,0 +1,19 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include <Database/Exceptions/column_parse_exception.h>
+
+#include <Database/Exceptions/column_type_mismatch_exception.h>
+
+#include <Database/Exceptions/connection_exception.h>
+
+#include <Database/Exceptions/constraint_exception.h>
+
+#include <Database/Exceptions/not_found_exception.h>
+
+#include <Database/Exceptions/query_exception.h>
+
+#include <Database/Exceptions/transaction_exception.h>
+
+
+#endif // EXCEPTIONS_H
