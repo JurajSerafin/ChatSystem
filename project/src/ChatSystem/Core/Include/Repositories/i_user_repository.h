3cc5d1ec8da7da80c3c3caf272235d04df4bbc7d
Juraj Serafin <79434395@cuni.cz>
2026-05-06 13:17:22 +0200

    Fix method signatures

--- project/src/ChatSystem/Core/Include/Repositories/i_user_repository.h
+++ project/src/ChatSystem/Core/Include/Repositories/i_user_repository.h
@@ -12,20 +12,22 @@
 class IUserRepository {
 public:
     virtual ~IUserRepository() = default;
-    virtual std::optional<User> FindById(UserId id) = 0;
+
+    virtual std::optional<User> FindById(const UserId& id) = 0;
+
     virtual std::optional<User> FindByLogin(const std::string& login) = 0;
+
     virtual std::optional<User> FindByTag(const std::string& tag) = 0;
 
     virtual std::vector<User> Search(const std::string& query) = 0;
 
     virtual User Create(const User& user) = 0;
-    virtual User Create(User&& user) = 0;
 
     virtual void Update(const User& user) = 0;
-    virtual void DeleteById(UserId id) = 0;
+    virtual void DeleteById(const UserId& id) = 0;
 
-    virtual void StorePublicKey(UserId id, const std::string& publicKey) = 0;
-    virtual std::optional<std::string> GetPublicKey(UserId id) = 0;
+    virtual void StorePublicKey(const UserId& id, const std::string& publicKey) = 0;
+    virtual std::optional<std::string> GetPublicKey(const UserId& id) = 0;
 };
 
 #endif // I_USER_REPOSITORY_H
