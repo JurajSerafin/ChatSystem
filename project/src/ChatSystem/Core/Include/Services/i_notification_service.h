a2113f3e558c76ba7a73dbef06b703a2fec8ac69
Juraj Serafin <79434395@cuni.cz>
2026-05-10 19:21:15 +0200

    Fix signatures

--- project/src/ChatSystem/Core/Include/Services/i_notification_service.h
+++ project/src/ChatSystem/Core/Include/Services/i_notification_service.h
@@ -21,7 +21,7 @@ public:
    * @param recipientId The target user to notify.
    * @param message The newly arrived message object.
    */
-  virtual void NotifyNewMessage(UserId recipientId, const Message& message) = 0;
+  virtual void NotifyNewMessage(const UserId& recipientId, const Message& message) = 0;
 
   /**
    * @brief Pushes a read receipt back to the original sender of a message.
@@ -30,19 +30,19 @@ public:
    * @param messageId The message that was just read.
    * @param readerId The user who triggered the read receipt.
    */
-  virtual void NotifyReadReceipt(UserId senderId, MessageId messageId, UserId readerId) = 0;
+  virtual void NotifyReadReceipt(const UserId& senderId, MessageId messageId, UserId readerId) = 0;
 
   /**
    * @brief Broadcasts to relevant contacts that a user has come online.
    * @param userId The user who established a WebSocket connection.
    */
-  virtual void NotifyUserOnline(UserId userId) = 0;
+  virtual void NotifyUserOnline(const UserId& userId) = 0;
 
   /**
    * @brief Broadcasts to relevant contacts that a user has gone offline.
    * @param userId The user whose WebSocket connection dropped.
    */
-  virtual void NotifyUserOffline(UserId userId) = 0;
+  virtual void NotifyUserOffline(const UserId& userId) = 0;
 };
 
 #endif // I_NOTIFICATION_SERVICE_H
