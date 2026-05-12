5b23746b4e265d0cf4a5bd153af88bc3cd8645bd
Juraj Serafin <79434395@cuni.cz>
2026-05-12 09:08:09 +0200

    Implement services

---
+++ project/src/ChatSystem/Server/Source/Services/chat_service.cpp
@@ -0,0 +1,137 @@
+#include "Chat/chat.h"
+#include "User/user.h"
+
+#include <Chat/chat_validator.h>
+#include <Services/chat_service.h>
+#include <algorithm>
+#include <stdexcept>
+
+ChatService::ChatService(IChatRepository* chatRepoObs, IUserRepository* userRepoObs)
+  : chat_repo_obs_(chatRepoObs), user_repo_obs_(userRepoObs) {
+  if (!chat_repo_obs_ || !user_repo_obs_) {
+    throw std::invalid_argument("ChatService requires valid IChatRepository and IUserRepository observers.");
+  }
+}
+
+
+void ChatService::EnforceParticipant(const ChatId& chatId, const UserId& userId) const {
+  if (!chat_repo_obs_->IsParticipant(chatId, userId)) {
+    throw std::logic_error("Access Denied: User is not a participant of this chat.");
+  }
+}
+
+void ChatService::EnforceUserExistence(const UserId& userId) const {
+  if (!user_repo_obs_->FindById(userId)) {
+    throw std::invalid_argument("User does not exist.");
+  }
+}
+
+Chat ChatService::TryFetchChat(const ChatId& chatId) const {
+  auto chat = chat_repo_obs_->FindById(chatId);
+
+  if (!chat) {
+    throw std::invalid_argument("Chat not found.");
+  }
+
+  return std::move(*chat);
+}
+
+
+void ChatService::EnforceUsersExistence(const std::vector<UserId>& userIds) const {
+  for (auto&& id : userIds) {
+    if (!user_repo_obs_->FindById(id)) {
+      throw std::invalid_argument("One or more participants do not exist.");
+    }
+  }
+
+}
+
+Chat ChatService::CreateNewChatType(const UserId& creatorId, const std::vector<UserId>& participantIds) {
+  std::vector all_participants{ creatorId };
+
+  all_participants.insert(all_participants.end(), participantIds.begin(), participantIds.end());
+
+  ChatParams params{
+      .id = ChatId::Generate(),
+      .created_at = std::chrono::system_clock::now(),
+      .type = CreateChatType<DirectChatType>(),
+      .last_message = std::nullopt,
+      .participant_ids = std::move(all_participants)
+  };
+
+  const ChatValidator validator;
+  Chat new_chat = Chat::Create(std::move(params), validator);
+
+  return chat_repo_obs_->Create(std::move(new_chat));
+}
+
+void ChatService::HandleSelfChat(const UserId& selfId, const UserId& otherId) {
+  if (selfId == otherId) {
+    throw std::invalid_argument("Cannot create a chat with yourself.");
+  }
+}
+
+Chat ChatService::CreateChat(const UserId& creatorId, const std::vector<UserId>& participantIds) {
+  if (participantIds.empty()) {
+    throw std::invalid_argument("Cannot create a chat without participants.");
+  }
+
+  EnforceUserExistence(creatorId);
+
+  EnforceUsersExistence(participantIds);
+
+  if (participantIds.size() == 1) {
+    HandleSelfChat(creatorId, participantIds[0]);
+
+    if (auto existing_chat = FindExistingChat<DirectChatType>(creatorId, participantIds[0])) {
+      return std::move(*existing_chat);
+    }
+  }
+
+  return CreateNewChatType(creatorId, participantIds);
+}
+
+std::vector<Chat> ChatService::GetChatsForUser(const UserId& userId, std::size_t limit, std::size_t offset) {
+  return chat_repo_obs_->FindByUserId(userId, limit, offset);
+}
+
+std::optional<Chat> ChatService::GetChatById(const ChatId& id, const UserId& requestingUserId) {
+  EnforceParticipant(id, requestingUserId);
+
+  return chat_repo_obs_->FindById(id);
+}
+
+std::vector<User> ChatService::FetchChatParticipants(const Chat& chat) const {
+  std::vector<User> participants;
+
+  for (auto&& user_id : chat.GetParticipantIds()) {
+    if (auto user = user_repo_obs_->FindById(user_id)) {
+      participants.emplace_back(std::move(*user));
+    }
+  }
+
+  return participants;
+}
+
+
+std::vector<User> ChatService::GetParticipants(const ChatId& id, const UserId& requestingUserId) {
+  const auto chat = TryFetchChat(id);
+
+  EnforceParticipant(id, requestingUserId);
+
+  return FetchChatParticipants(chat);
+}
+
+void ChatService::DeleteChat(const ChatId& id, const UserId& requestingUserId) {
+  EnforceParticipant(id, requestingUserId);
+
+  chat_repo_obs_->DeleteById(id);
+}
+
+void ChatService::AddParticipant(const ChatId& id, const UserId& requestingUserId, const UserId& newUserId) {
+  throw std::logic_error("AddParticipant is not yet supported.");
+}
+
+void ChatService::RemoveParticipant(const ChatId& id, const UserId& requestingUserId, const UserId& userIdToRemove) {
+  throw std::logic_error("RemoveParticipant is not yet supported.");
+}
---
+++ project/src/ChatSystem/Server/Source/Services/message_service.cpp
@@ -0,0 +1,116 @@
+#include <Services/message_service.h>
+
+#include "TypeLibHelpers/domain_class_type_variant_factory.h"
+#include <stdexcept>
+#include <format>
+
+inline MessageService::MessageService(
+  INotificationService* notificationServiceObs,
+  IMessageRepository* messageRepoObs,
+  IChatRepository* chatRepoObs
+) : notification_service_obs_(notificationServiceObs), message_repo_obs_(messageRepoObs), chat_repo_obs_(chatRepoObs) {
+  if (notification_service_obs_ || message_repo_obs_ || chat_repo_obs_) {
+    throw std::invalid_argument(
+      "MessageService requires valid INotificationService,IMessageRepository and IChatRepository observers.");
+  }
+}
+
+void MessageService::EnforceParticipant(const ChatId& chatId, const UserId& userId) const {
+  if (!chat_repo_obs_->IsParticipant(chatId, userId)) {
+    throw std::logic_error(std::format(
+      "User with id {} is not a participant of the chat wit id {}", userId.ToString(), chatId.ToString())
+    );
+  }
+}
+void MessageService::EnforceChatExistence(const ChatId& chatId) const {
+  if (const auto chat = chat_repo_obs_->FindById(chatId); !chat) {
+    throw std::logic_error(std::format("Chat with id {} does not exist.", chatId.ToString()));
+  }
+}
+
+Message MessageService::SendMessage(
+  const UserId& senderId,
+  const ChatId& chatId,
+  const std::string& ciphertext,
+  const EncryptedKeysMap& encryptedKeys,
+  MessageTypeVariant type) {
+
+  EnforceChatExistence(chatId);
+
+  EnforceParticipant(chatId, senderId);
+
+  MessageParams msg_params{
+    .id = MessageId::Generate(),
+    .chat_id = chatId,
+    .sender_id = senderId,
+    .ciphertext = ciphertext,
+    .type = type,
+    .created_at = std::chrono::system_clock::now(),
+  };
+
+  auto saved_msg = message_repo_obs_->Save(Message::Create(std::move(msg_params), msg_validator_), encryptedKeys);
+
+  chat_repo_obs_->UpdateLastMessage(chatId, saved_msg);
+
+  auto chat = chat_repo_obs_->FindById(chatId);
+
+  for (auto&& participant_id : chat->GetParticipantIds()) {
+    if (participant_id != senderId) {
+      notification_service_obs_->NotifyNewMessage(participant_id, saved_msg);
+    }
+  }
+
+  return saved_msg;
+}
+
+std::optional<std::string> MessageService::GetEncryptedKey(const MessageId& messageId, const UserId& userId) {
+  return message_repo_obs_->GetEncryptedKey(messageId, userId);
+}
+
+std::vector<Message> MessageService::GetHistory(const ChatId& chatId, const UserId& requestingUserId, std::size_t limit, std::size_t offset) {
+  EnforceChatExistence(chatId);
+
+  EnforceParticipant(chatId, requestingUserId);
+
+  return message_repo_obs_->FindByChatId(chatId, limit, offset);
+}
+
+void MessageService::MarkAsRead(const MessageId& id, const UserId& readerId) {
+  const auto msg = message_repo_obs_->FindById(id);
+
+  if (!msg) {
+    throw std::invalid_argument("Message not found.");
+  }
+
+  if (msg->GetSenderId() == readerId) {
+    return;
+  }
+
+  message_repo_obs_->MarkRead(id, readerId);
+
+  notification_service_obs_->NotifyReadReceipt(msg->GetSenderId(), id, readerId);
+}
+
+std::vector<Message> MessageService::GetUndelivered(const UserId& userId) {
+  auto missed_messages = message_repo_obs_->FindUndelivered(userId);
+
+  for (auto&& msg : missed_messages) {
+    message_repo_obs_->MarkDelivered(msg.GetId(), userId);
+  }
+
+  return missed_messages;
+}
+
+void MessageService::DeleteMessage(const MessageId& id, const UserId& requesterId) {
+  const auto msg = message_repo_obs_->FindById(id);
+
+  if (!msg) {
+    throw std::invalid_argument("Message not found.");
+  }
+
+  if (msg->GetSenderId() != requesterId) {
+    throw std::logic_error("You are not allowed delete someone else's message.");
+  }
+
+  message_repo_obs_->DeleteById(id);
+}
