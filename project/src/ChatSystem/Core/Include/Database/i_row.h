11abd4fb804b8a81acac7904462603dd4e35c7df
Juraj Serafin <79434395@cuni.cz>
2026-05-12 09:10:52 +0200

    Add =0 to pure virtual methods

--- project/src/ChatSystem/Core/Include/Database/i_row.h
+++ project/src/ChatSystem/Core/Include/Database/i_row.h
@@ -5,15 +5,17 @@
 
 class IRow {
 public:
-  [[nodiscard]] virtual std::string GetString(const char* columnName) const;
+  virtual ~IRow() = default;
 
-  [[nodiscard]] virtual int GetInt(const char* columnName) const;
+  [[nodiscard]] virtual std::string GetString(const char* columnName) const = 0;
 
-  [[nodiscard]] virtual std::string GetUuid(const char* columnName) const;
+  [[nodiscard]] virtual int GetInt(const char* columnName) const = 0;
 
-  [[nodiscard]] virtual std::chrono::system_clock::time_point GetTimeStamp(const char* columnName) const;
+  [[nodiscard]] virtual std::string GetUuid(const char* columnName) const = 0;
 
-  [[nodiscard]] virtual bool IsNull(const char* columnName) const;
+  [[nodiscard]] virtual std::chrono::system_clock::time_point GetTimeStamp(const char* columnName) const = 0;
+
+  [[nodiscard]] virtual bool IsNull(const char* columnName) const = 0;
 }; 
 
 #endif // I_ROW_H
