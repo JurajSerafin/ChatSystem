fb7e00388a18701f57ce880c8c4ae1c3561aa89a
Juraj Serafin <79434395@cuni.cz>
2026-05-16 17:31:59 +0200

    Create local database interface

---
+++ project/src/ChatSystem/App/Include/Database/i_local_database.h
@@ -0,0 +1,58 @@
+#ifndef I_LOCAL_DATABASE_H
+#define I_LOCAL_DATABASE_H
+
+#include "User/Roles/user_role_variant.h"
+#include "local_db_models.h"
+
+#include <optional>
+#include <vector>
+#include <string_view>
+
+class ILocalDatabase {
+public:
+  virtual ~ILocalDatabase() = default;
+
+  virtual void SaveIdentity(const CachedIdentity& identity) = 0;
+
+  virtual std::optional<CachedIdentity> LoadIdentity() = 0;
+
+  virtual void ClearIdentity() = 0;
+
+
+  virtual void UpsertUser(const CachedUser& user) = 0;
+
+  virtual std::optional<CachedUser> LoadUser(std::string_view userId) = 0;
+
+
+  virtual void UpsertChat(const CachedChat& chat) = 0;
+
+  virtual std::optional<CachedChat> LoadChat(std::string_view chatId) = 0;
+
+  virtual std::vector<CachedChat> LoadChats(std::size_t limit, std::size_t offset) = 0;
+
+  virtual void AddUserToChat(std::string_view userId, std::string_view chatId, const UserRoleVariant& chatRole) = 0;
+
+  virtual void SetUserChatRole(std::string_view userId, std::string_view chatId, const UserRoleVariant& chatRole) = 0;
+
+  virtual std::optional<UserRoleVariant> GetUserChatRole(std::string_view userId, std::string_view chatId) = 0;
+
+  virtual std::vector<std::string> GetChatParticipantIds(std::string_view chatId) = 0;
+
+
+  virtual void SaveMessageForChat(const CachedMessage& message) = 0;
+
+  virtual std::vector<CachedMessage> GetMessagesForChat(
+    std::string_view chatId,
+    std::size_t limit,
+    std::size_t offset
+  ) = 0;
+
+  virtual void MarkMessageAsRead(std::string_view messageId) = 0;
+
+
+  virtual void EvictOldMessages(std::chrono::system_clock::time_point olderThanTimeStamp) = 0;
+
+  virtual void NukeEntireCache() = 0;
+};
+
+#endif // I_LOCAL_DATABASE_H
---
+++ project/src/ChatSystem/App/Include/Database/local_db_models.h
@@ -0,0 +1,47 @@
+#ifndef LOCAL_DB_MODELS_H
+#define LOCAL_DB_MODELS_H
+
+#include <string>
+#include <optional>
+#include <chrono>
+
+struct CachedIdentity {
+  std::string login;
+  std::string id;
+  std::string tag;
+  std::string session_token;
+};
+
+struct CachedUser {
+  std::string login;
+  std::string id;
+  std::string tag;
+  std::string public_key;
+
+  std::chrono::system_clock::time_point cached_at;
+};
+
+struct CachedChat {
+  std::string id;
+
+  std::optional<std::string> last_message_id;
+  std::optional<std::string> name;
+
+  std::chrono::system_clock::time_point last_activity_at;
+  std::chrono::system_clock::time_point cached_at;
+};
+
+struct CachedMessage {
+  std::string id;
+  std::string sender_id;
+  std::string chat_id;
+  std::string plaintext;
+  std::string type;
+
+  std::chrono::system_clock::time_point created_at;
+
+  bool is_read;
+  bool is_delivered;
+};
+
+#endif // LOCAL_DB_MODELS_H
