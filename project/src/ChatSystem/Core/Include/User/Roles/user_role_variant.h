65842fa75ef856e1ca063a5c969ffdff659a04af
Juraj Serafin <79434395@cuni.cz>
2026-05-16 18:37:12 +0200

    Create string -> UserRoleVariantconversion helpers

--- project/src/ChatSystem/Core/Include/User/Roles/user_role_variant.h
+++ project/src/ChatSystem/Core/Include/User/Roles/user_role_variant.h
@@ -5,6 +5,8 @@
 #include "user_role.h"
 #include "user_role_validator.h"
 
+#include <string_view>
+#include <string>
 #include <variant>
 
 using UserRoleVariant = std::variant<
@@ -12,5 +14,11 @@ using UserRoleVariant = std::variant<
 >;
 
 static_assert(AllUserRolesValid<UserRoleVariant>, "Role not valid, make sure it is compliant with UserRole concept.");
+
+namespace UserRoles {
+  std::string RoleToString(const UserRoleVariant& roleVar);
+
+  UserRoleVariant StringToRole(std::string_view roleStr);
+} // UserRoles
 
 #endif // USER_ROLE_VARIANT_H
