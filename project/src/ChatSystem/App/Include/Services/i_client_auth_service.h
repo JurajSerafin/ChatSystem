68101b33db7e7520f34401a191b122a7c4896034
Juraj Serafin <79434395@cuni.cz>
2026-05-17 20:24:04 +0200

    Fix return type

--- project/src/ChatSystem/App/Include/Services/i_client_auth_service.h
+++ project/src/ChatSystem/App/Include/Services/i_client_auth_service.h
@@ -1,16 +1,18 @@
 #ifndef I_CLIENT_AUTH_SERVICE_H
 #define I_CLIENT_AUTH_SERVICE_H
 
+#include "Database/local_db_models.h"
 #include "User/user.h"
+
 #include <string_view>
 
 class IClientAuthService {
 public:
   virtual ~IClientAuthService() = default;
 
-  virtual User Register(std::string_view login, std::string_view password) = 0;
+  virtual CachedUser Register(std::string_view login, std::string_view password) = 0;
 
-  virtual User Login(std::string_view login, std::string_view password) = 0;
+  virtual CachedUser Login(std::string_view login, std::string_view password) = 0;
 
   virtual void Logout() = 0;
 };
