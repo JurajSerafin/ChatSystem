d5208cfcc32edac5c89664ecd0716aae23fd3c0d
Juraj Serafin <79434395@cuni.cz>
2026-05-17 16:09:36 +0200

    Create cryptography fasade

---
+++ project/src/ChatSystem/App/Include/Cryptography/i_client_key_manager.h
@@ -0,0 +1,18 @@
+#ifndef I_CLIENT_KEY_MANAGER_H
+#define I_CLIENT_KEY_MANAGER_H
+
+#include "key_pair.h"
+#include <string_view>
+
+class IClientKeyManager {
+public:
+  virtual void ~IClientKeyManager() = default;
+
+  virtual KeyPair GenerateAndProtectKeyPair(std::string_view password) = 0;
+
+  virtual std::string GetUnlockedPrivateKey() = 0;
+
+  virtual void DeleteProtectedKeys() = 0;
+};
+
+#endif // I_CLIENT_KEY_MANAGER_H
